8b4364ef321f66af5a48062ffbf256f8ef51a417
Priya Dharshini S <priyaseenivasan20097@gmail.com>
2026-09-21 20:36:08 +0530

    Fix CORS for live portfolio

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+app.use(cors({
+    origin: "https://priya-dharshini-portfolioo.netlify.app"
+}));
 
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
